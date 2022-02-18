283598fc0afc40c5296a8fbf861148efff258d95
zachary <zacharyparks@protonmail.com>
2022-02-17 23:24:00 -0500

    new game enhancements

--- hello.py
+++ hello.py
@@ -4,4 +4,8 @@ print("| |_   ___ | || | ___        _ __ __  ___  _ _ | | __| | | |  ")
 print("|   \ / -_)| || |/ _ \       \ V  V // _ \| '_|| |/ _` | |_|  ")
 print("|_||_|\___||_||_|\___/        \_/\_/ \___/|_|  |_|\__/_| (_)  ")
 
+input("Don't press the enter button...")
+for i in range(50):
+    print("I'm going to break your computer with an infinite loop!")
 
+print("Did I scare you?")
