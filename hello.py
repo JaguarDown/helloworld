c4bc9cd8cb24c63b6e1d469f14a2b0f9bdf297f4
zachary <zacharyparks@protonmail.com>
2022-02-17 23:31:21 -0500

    combat upgrade, fixes #1

--- hello.py
+++ hello.py
@@ -4,6 +4,8 @@ print("| |_   ___ | || | ___        _ __ __  ___  _ _ | | __| | | |  ")
 print("|   \ / -_)| || |/ _ \       \ V  V // _ \| '_|| |/ _` | |_|  ")
 print("|_||_|\___||_||_|\___/        \_/\_/ \___/|_|  |_|\__/_| (_)  ")
 
+print("Swish swish, clang clang")
+
 input("Don't press the enter button...")
 for i in range(50):
     print("I'm going to break your computer with an infinite loop!")
